b7033cdc6bd3b53ca9cc1a818b5fd3988418dc6f
Vasco Sousa <email@vascosousa.com>
2023-05-09 13:03:19 +0200

    added files to repo

---
+++ gen/com/ecloset/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ecloset;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/ecloset/EClosetActivity.java
@@ -0,0 +1,13 @@
+package com.ecloset;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EClosetActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
